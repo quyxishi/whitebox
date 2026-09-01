3da3b59d1fba8084ecb5770654c0bda7ea8a6f30
quyxishi <78678868+quyxishi@users.noreply.github.com>
2026-09-01 21:39:17 +0500

    style: rename the CLI listen address field to follow the naming pattern

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -106,7 +106,7 @@ func main() {
 
 	// *
 
-	server := api.NewServer(wrapper, cli.ListenAddr)
+	server := api.NewServer(wrapper, cli.WebListenAddr)
 
 	// Create a done channel to signal when the shutdown is complete.
 	done := make(chan bool, 1)
